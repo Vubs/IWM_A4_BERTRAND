ef7c7894977736456b406a1839b1668b924ac966
Vincent Bertrand <b.vincent.iim@gmail.com>
2017-10-12 16:19:59 +0200

    Add fillable name to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,15 @@ class Product extends Model
 {
     /*protected $foreignKey = "prodcuts_type_id";*/
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
     public function productType()
     {
         return $this->belongsTo('App\Models\ProductType', 'products_type_id');
